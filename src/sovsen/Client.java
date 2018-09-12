3afe2a953644d2c4577a782faaa3ce6f0e755034
niel905g <niel905g@edu.easj.dk>
2018-09-12 19:08:32 +0200

    Server and Client socket connection ready....

--- src/sovsen/Client.java
+++ src/sovsen/Client.java
@@ -1,7 +1,17 @@
 package sovsen;
-
+import java.io.IOException;
+import java.net.*;
 /**
  * @Author SovsenGrp 12-Sep-18.
  */
 public class Client {
+    public static void main(String[] args) {
+
+        try {
+            Socket s = new Socket("127.0.0.1", 3000);
+            System.out.println("Connected!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/sovsen/Server.java
+++ src/sovsen/Server.java
@@ -1,8 +1,25 @@
 package sovsen;
-
+import java.io.IOException;
+import java.net.*;
 /**
  * @Author SovenGrp on 12-Sep-18.
  */
 public class Server {
-    
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(3000);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            try {
+                Socket s=server.accept();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            System.out.println("Connected");
+        }
+    }
 }
