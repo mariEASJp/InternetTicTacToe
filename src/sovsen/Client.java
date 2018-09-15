9c9851bdf852cca2d529647cd763497c74cbcb3f
Trick <maria.olsen22@live.dk>
2018-09-15 17:30:44 +0200

    Client now continuously listens for Server input

--- src/sovsen/Client.java
+++ src/sovsen/Client.java
@@ -1,8 +1,6 @@
 package sovsen;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 /**
@@ -14,11 +12,45 @@ public class Client {
         try {
             Socket s = new Socket("127.0.0.1",3001);
 
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            String msg = dis.readUTF();
+           // DataInputStream dis = new DataInputStream(s.getInputStream());
+           // String msg = dis.readUTF();
 
-            System.out.println(msg);
+           // System.out.println(msg);
+
+
+            String fromServer;
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader((System.in)));
+
+
+            while ((fromServer = in.readLine()) != null){
+                System.out.println("Server: " + fromServer);
+
+                if (fromServer.equals("End")){
+                    System.out.println("Server is silent");
+                    break;
+                }
+
+
+                String fromUser = stdIn.readLine();
+                if (fromUser != null){
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+
+            }
+
+
+
+            //Close the connection and exit
+            // dis.close();
+            //s1In.close();
+            //s1.close();
+
+
         } catch (Exception e) {
+            System.out.println(e.getStackTrace());
         }
     }
 }
