1142834025a42fe425ed90b043add3622d64abd6
Trick <maria.olsen22@live.dk>
2018-09-16 05:54:47 +0200

    Added new Client code

--- src/sovsen/Client.java
+++ src/sovsen/Client.java
@@ -7,25 +7,37 @@ import java.net.*;
  * @Author SovsenGrp 12-Sep-18.
  */
 public class Client {
+
+    static Socket s1;
+    static BufferedReader stdIn;
+
     public static void main(String[] args) {
 
+
         try {
-            Socket s = new Socket("127.0.0.1",3001);
 
-           // DataInputStream dis = new DataInputStream(s.getInputStream());
-           // String msg = dis.readUTF();
-
-           // System.out.println(msg);
+            s1 = new Socket("localhost", 3001);
 
 
-            String fromServer;
-            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader((System.in)));
+            String fromServer = "";
+            BufferedReader in = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+
+            stdIn = new BufferedReader(new InputStreamReader((System.in)));
 
 
             while ((fromServer = in.readLine()) != null){
-                System.out.println("Server: " + fromServer);
+
+                System.out.println("Conn");
+                String[] strArr = fromServer.split("~");
+
+
+
+
+                System.out.println("Server: ");
+
+                for (String c : strArr) {
+                    System.out.println("Client-c " + c + "\t");
+                }
 
                 if (fromServer.equals("End")){
                     System.out.println("Server is silent");
@@ -33,24 +45,48 @@ public class Client {
                 }
 
 
+
                 String fromUser = stdIn.readLine();
+
                 if (fromUser != null){
+
+                    write("from user: " + fromUser);
                     System.out.println("Client: " + fromUser);
-                    out.println(fromUser);
+                    // out.println(fromUser);
                 }
+
+
+
 
             }
 
 
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println(ex.getStackTrace());
+        }
 
-            //Close the connection and exit
-            // dis.close();
-            //s1In.close();
-            //s1.close();
 
 
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
+        //Close the connection and exit
+        // dis.close();
+        //s1In.close();
+        //s1.close();
+    }
+
+
+    public static void write(String fromUser){
+        try {
+
+            PrintWriter out;
+            out = new PrintWriter(s1.getOutputStream(), true);
+            out.println(fromUser);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+
         }
     }
 }
